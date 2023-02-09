87ea66688dc8488b34df3cd68b0d63da12a207db
ronmsls <99310138+ronmsls@users.noreply.github.com>
2023-02-09 04:32:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(res => console.log('Registro de SW exitoso', reg))
-      .catch(err => console.log("service worker not registered", err))
-  })
+var base_url="/";
+console.log('Solicitud Service WORKER');
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register(base_url+'sw.js')
+    .then(reg => console.log('Registro de SW exitoso', reg))
+    .catch(err => console.warn('Error al tratar de registrar el sw', err)) 
 }
