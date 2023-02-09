092342ca74165fc28ddc42629a0dd326c8f1c419
ronmsls <99310138+ronmsls@users.noreply.github.com>
2023-02-09 00:50:18 -0500

    Update Loader.php

--- system/core/Loader.php
+++ system/core/Loader.php
@@ -50,7 +50,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @link		https://codeigniter.com/userguide3/libraries/loader.html
  */
 class CI_Loader {
-
+ public $load;
+    public $benchmark;
+    public $config;
+    public $log;
+    public $hooks;
+    public $utf8;
+    public $uri;
+    public $router;
+    public $exceptions;
+    public $output;
+    public $security;
+    public $input;
+    public $lang;
 	// All these are set automatically. Don't mess with them.
 	/**
 	 * Nesting level of the output buffering mechanism
