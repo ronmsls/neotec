0aeb91257898d209ae9b29761702fd3e885cf7ab
ronmsls <99310138+ronmsls@users.noreply.github.com>
2023-02-09 02:15:01 -0500

    Update Clientes.php

--- application/controllers/Clientes.php
+++ application/controllers/Clientes.php
@@ -8,7 +8,6 @@
               $this->load->model("plan");
               $this->load->model("cobro");
               $this->load->model("ip");
-              $this->load->library('conectar');  
               // validando si alguien esta conectado
           if ($this->session->userdata("usuario_Conectado")) { 
             // si esta conectado
@@ -97,7 +96,7 @@
             if($this->cliente->insertar($datosNuevoCliente)){
                 $this->session->set_flashdata('confirmacion','usuario insertado exitosamente'); 
                 //registro del cliente en mikrotik
-                $this->conectarmikrotik->conectar($direccion_ip,$nombreCompleto,$mg,$cedula);
+                conectar($direccion_ip,$nombreCompleto,$mg,$cedula);
                 //actualizar el estado de la ip 
                 $datosIp=array(
                     "estado_ip"=>$this->input->post("0")
@@ -185,6 +184,40 @@
 
         }
       }
+		function conectar($target, $nameCliente, $maxlimit, $comment){   
+        $ipRouteros="170.244.209.28";  // tu RouterOS.
+        $Username="api-tesis";
+        $Pass="12345678";
+        $api_puerto=8798;
+        $API = new RouterosAPI();
+        $API->debug = false;
+        if ($API->connect($ipRouteros , $Username , $Pass, $api_puerto)) {  
+            $API->write("/queue/simple/getall",false);
+            $API->write('?comment='.$comment,true);
+            $READ = $API->read(false);
+            $ARRAY = $API->parseResponse($READ);
+             if(count($ARRAY)>0){ // si el nombre de usuario "ya existe" lo edito
+                $API->write("/queue/simple/set",false);
+                $API->write("=.id=".$ARRAY[0]['.id'],false);
+                $API->write('=target='.$target,false);
+                $API->write('=name='.$nameCliente,false);
+                $a=$maxlimit*1000;
+                $API->write('=max-limit='."$a/$a",true);    //   2M/2M   [TX/RX]
+                $READ = $API->read(true);
+                $ARRAY = $API->parseResponse($READ);
+             }else{
+                 $API->write("/queue/simple/add",false);
+                 $API->write('=target='.$target,false);   // IP
+                 $API->write('=name='.$nameCliente,false);       // nombre
+                 $API->write('=max-limit='.$maxlimit."M",false);   //   2M/2M   [TX/RX]
+                 $API->write('=comment='.$comment,true);         // comentario
+                 $READ = $API->read(false);
+                 $ARRAY = $API->parseResponse ($READ);
+             }   
+         
+         }
+         $API->disconnect();
+           }
 
     }
 
