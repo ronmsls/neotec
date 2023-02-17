dd012336c0934c3922081a8149ad47bfa4bfe0aa
ronmsls <99310138+ronmsls@users.noreply.github.com>
2023-02-16 23:09:35 -0500

    Update dashboardVista.php

--- application/views/dashboard/dashboardVista.php
+++ application/views/dashboard/dashboardVista.php
@@ -8,7 +8,7 @@
                 <div class="row g-4">
                     <div class="col-sm-12 col-xl-12">
                         <div class="bg-secondary rounded h-100 p-4" style="align: center;">
-                        <center><img src="<?php echo base_url(); ?>/assets/img/logo.png" alt="" style="width: 70%;"></center>
+                        <center><img src="<?php echo base_url(); ?>/assets/img/Logo.png" alt="" style="width: 70%;"></center>
                         <br>
                         <center>
                         <div class="border rounded p-4 pb-0 mb-4">
