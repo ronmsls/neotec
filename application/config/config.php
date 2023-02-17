19f551ea3c9f206669359e35cd234fb793781a26
ronmsls <99310138+ronmsls@users.noreply.github.com>
2023-02-17 02:01:12 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://localhost/neotec/';
+$config['base_url'] = 'https://ok1.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
