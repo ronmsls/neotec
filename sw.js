7fdd290791eba24c2d4f41f8f34e02e48ff09f90
ronmsls <99310138+ronmsls@users.noreply.github.com>
2023-02-17 02:55:24 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,25 +2,7 @@
 const CACHE_NAME = 'Neotec_10',
   urlsToCache = [
    	'<?php echo base_url();?>',
-		'<?php echo base_url(); ?>/assets/css/avatar.png',
-	  '<?php echo base_url(); ?>/assets/img/favicon.png',
-	  '<?php echo base_url(); ?>/assets/lib/owlcarousel/assets/owl.carousel.min.css',
-	  '<?php echo base_url(); ?>/assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css',
-	  '<?php echo base_url(); ?>/assets/css/bootstrap.min.css',
-	  '<?php echo base_url(); ?>/assets/css/style.css',
-	  '<?php echo base_url(); ?>/assets/css/styleLogin.css',
-	  '<?php echo base_url(); ?>/assets/css/avatar.png',
-	    '<?php echo base_url(); ?>/assets/lib/chart/chart.min.js',
-    	'<?php echo base_url(); ?>/assets/lib/easing/easing.min.js',
-   ' <?php echo base_url(); ?>/assets/lib/waypoints/waypoints.min.js',
-    '<?php echo base_url(); ?>/assets/lib/owlcarousel/owl.carousel.min.js',
-    '<?php echo base_url(); ?>/assets/lib/tempusdominus/js/moment.min.js',
-    '<?php echo base_url(); ?>/assets/lib/tempusdominus/js/moment-timezone.min.js',
-    '<?php echo base_url(); ?>/assets/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js>',
-	  'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,600,0,0',
-    'https://fonts.googleapis.com',
-    'https://fonts.gstatic.com',
-    'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap'
+	'<?php echo base_url(); ?>/views/dashboard/*'
   ]
 
 //durante la fase de instalación, generalmente se almacena en caché los activos estáticos
