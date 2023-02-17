9fb1c4fbaccb11cc607422b4188a641e237dd6b5
ronmsls <99310138+ronmsls@users.noreply.github.com>
2023-02-17 02:22:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('https://ok1.herokuapp.com//sw.js').then(function(registration) {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
       // Registro exitoso
       console.log('ServiceWorker registrado exitosamente con scope: ', registration.scope);
     }, function(err) {
