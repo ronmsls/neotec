c277b8cc16095a3e0342366155b5a7151ecd73b5
ronmsls <99310138+ronmsls@users.noreply.github.com>
2023-02-17 02:07:01 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,54 +1,58 @@
 //asignar un nombre y versión al cache
-var CACHE_NAME = 'mi-cache-v4';
-var urlsToCache = [
+const CACHE_NAME = 'Neotec_1',
+  urlsToCache = [
     './',
     'https://fonts.googleapis.com/css?family=Raleway:400,700',
     'https://fonts.gstatic.com/s/raleway/v12/1Ptrg8zYS_SKggPNwJYtWqZPAA.woff2',
     'https://use.fontawesome.com/releases/v5.0.7/css/all.css',
     'https://use.fontawesome.com/releases/v5.0.6/webfonts/fa-brands-400.woff2',
-    '/css/style.css',
-    '/js/app.js',
-    '/img/logo.png'
-  ] 
+  ]
 
-  self.addEventListener('install', function(event) {
-    // Realizar la instalación del service worker y almacenar en caché los recursos necesarios.
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          console.log('Abriendo cache');
-          return cache.addAll(urlsToCache);
-        })
-    );
-  });
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting())
+      })
+      .catch(err => console.log('Falló registro de cache', err))
+  )
+})
 
-  self.addEventListener('fetch', function(event) {
-    // Atender peticiones y devolver los recursos desde la caché o desde la red.
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          if (response) {
-            return response;
-          }
-  
-          var fetchRequest = event.request.clone();
-  
-          return fetch(fetchRequest).then(
-            function(response) {
-              if(!response || response.status !== 200 || response.type !== 'basic') {
-                return response;
-              }
-  
-              var responseToCache = response.clone();
-  
-              caches.open(CACHE_NAME)
-                .then(function(cache) {
-                  cache.put(event.request, responseToCache);
-                });
-  
-              return response;
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME]
+
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
             }
-          );
-        })
-    );
-  });
+          })
+        )
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  )
+})
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      })
+  )
+})
