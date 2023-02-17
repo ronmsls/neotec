47d5b8441ad482d533171c30036f78864c35b9b5
ronmsls <99310138+ronmsls@users.noreply.github.com>
2023-02-17 02:41:57 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'Neotec_10',
   urlsToCache = [
    	'<?php echo base_url();?>',
-		'<?php echo base_url(); ?>/assets/img/',
+		'<?php echo base_url(); ?>/assets/img/'
   ]
 
 //durante la fase de instalación, generalmente se almacena en caché los activos estáticos
