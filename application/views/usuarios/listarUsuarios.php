a61f22daed7d74f8289e454a78ff2985cb119007
ronmsls <99310138+ronmsls@users.noreply.github.com>
2023-02-17 12:44:15 -0500

    Update listarUsuarios.php

--- application/views/usuarios/listarUsuarios.php
+++ application/views/usuarios/listarUsuarios.php
@@ -84,9 +84,6 @@
                                         <th class="text-center">CELULAR USUARIO</th>
                                         <th class="text-center">CORREO USUARIO</th>
                                         <th class="text-center"></th>
-                                        <?php if ($this->session->userdata("usuario_Conectado")["rol"]=="ADMINISTRADOR_ROOT"): ?>
-                                        <th></th>
-                                        <?php endif; ?>
                                       </tr>
                                     </thead>
                                     <tbody>
@@ -111,11 +108,6 @@
                                           <td class="text-center">
                                               <a class="btn btn-success"  href="<?php echo site_url(); ?>/usuarios/editarUsuario/<?php echo $filaTemporal->id_usuario; ?>/<?php echo $filaTemporal->fk_id_rol; ?>" > <i class="fa fa-pen"></i></a>
                                           </td>
-                                          <?php if ($this->session->userdata("usuario_Conectado")["rol"]=="ADMINISTRADOR_ROOT"): ?>
-                                          <td>
-                                          <a class="btn btn-danger" href='javascript:void(0)' onclick="confirmarEliminacion('<?php echo$filaTemporal->id_usuario; ?>');"><i class="fa fa-trash"></i>                                          
-                                        </td>
-                                          <?php endif; ?>
                                         </tr>
                                       <?php endforeach; ?>
                                     </tbody>
