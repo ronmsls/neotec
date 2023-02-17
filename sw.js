e2bae0dd9373b18c6b53e3715053ceff32cc282a
ronmsls <99310138+ronmsls@users.noreply.github.com>
2023-02-17 03:33:25 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,59 +1,24 @@
-//asignar un nombre y versión al cache
-const CACHE_NAME = 'Neotec_1'6,
-  urlsToCache = [
-   	'<?php echo base_url();?>',
-	  '<?php echo base_url();?>/assets/css/styleLogin.css',
-	'<?php echo base_url(); ?>/views/seguridades/login.php',
-	'<?php echo base_url(); ?>/views/dashboard/dashboardView.php',
-	'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,600,0,0'
-	 
-  ]
+const CACHE_NAME = 'my-cache-v1';
 
-//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache)
-          .then(() => self.skipWaiting())
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Clonar la respuesta para que podamos guardarla en caché
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseClone);
+          });
+
+        return response;
       })
-      .catch(err => console.log('Falló registro de cache', err))
-  )
-})
-
-//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
-self.addEventListener('activate', e => {
-  const cacheWhitelist = [CACHE_NAME]
-
-  e.waitUntil(
-    caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            //Eliminamos lo que ya no se necesita en cache
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName)
-            }
-          })
-        )
+      .catch(() => {
+        return caches.match(event.request)
+          .then(response => {
+            return response || new Response(null, {status: 404});
+          });
       })
-      // Le indica al SW activar el cache actual
-      .then(() => self.clients.claim())
-  )
-})
-
-//cuando el navegador recupera una url
-self.addEventListener('fetch', e => {
-  //Responder ya sea con el objeto en caché o continuar y buscar la url real
-  e.respondWith(
-    caches.match(e.request)
-      .then(res => {
-        if (res) {
-          //recuperar del cache
-          return res
-        }
-        //recuperar de la petición a la url
-        return fetch(e.request)
-      })
-  )
-})
+  );
+});
